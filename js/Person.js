4e6e84c740774756067594c09212dfee67d9450e
Felipe <feliperuppel@hotmail.com>
2013-07-20 17:49:24 -0300

    Correção na animação de Person

--- js/Person.js
+++ js/Person.js
@@ -18,6 +18,8 @@
     p.randomSpeedForWalk;
     p.randomTimeForWalk;
     p.countTimeForWalk = 0;
+    
+    p.lastDirection;
 
     // constructor:
     p.Container_initialize = p.initialize; //unique to avoid overiding base class
@@ -32,8 +34,13 @@
             frames: {width: 60, height: 120, regX: 30, regY: 60},
             animations: {
                 down: [0, 11, "down", 4],
-                down_l: [0, 11, "down_l", 4],
-                down_r: [0, 11, "down_r", 4]
+                up: [0, 11, "up", 4],
+                left: [0, 11, "left", 4],
+	            right: [0, 11, "right", 4],
+	            stoped_down: [0, 11, "stopped_down", 4],
+	            stoped_up: [0, 11, "stopped_up", 4],
+	            stoped_left: [0, 11, "stopped_left", 4],
+	            stoped_right: [0, 11, "stopped_right", 4],
             }
         });
 
@@ -68,21 +75,53 @@
 			this.countTimeForWalk = 0;
 		}
     	
-    	if(this.randomDirection < 8){
-	    	if(this.randomDirection <= 1){
-	    		this.x += this.randomSpeedForWalk;
-	    		this.gotoAndPlay("down_r");
-			}else if(this.randomDirection <= 3){
-				this.y += this.randomSpeedForWalk;
-				this.gotoAndPlay("down");
-			}else if(this.randomDirection <= 5){
-				this.x -= this.randomSpeedForWalk;
-				this.gotoAndPlay("down_l");
-			}else if(this.randomDirection <= 7){
-				this.y -= this.randomSpeedForWalk;
-				this.gotoAndPlay("down");
+    	if(this.randomDirection <= 1){
+    		this.x += this.randomSpeedForWalk;
+    		
+    		if(this.lastDirection != "right"){
+    			this.gotoAndPlay("right");
+    		}
+    		
+    		this.lastDirection = "right";
+    		
+		}else if(this.randomDirection <= 3){
+			this.y += this.randomSpeedForWalk;
+			
+			if(this.lastDirection != "up"){
+    			this.gotoAndPlay("up");
+    		}
+			this.lastDirection = "up";
+			
+		}else if(this.randomDirection <= 5){
+			this.x -= this.randomSpeedForWalk;
+			if(this.lastDirection != "left"){
+    			this.gotoAndPlay("left");
+    		}
+			this.lastDirection = "left";
+		}else if(this.randomDirection <= 7){
+			this.y -= this.randomSpeedForWalk;
+			if(this.lastDirection != "down"){
+    			this.gotoAndPlay("down");
+    		}
+			this.lastDirection = "down";
+		}else{
+			switch(this.lastDirection){
+			case "right":
+				this.gotoAndPlay("stopped_right");
+				break;
+			case "left":
+				this.gotoAndPlay("stopped_left");
+				break;
+			case "up":
+				this.gotoAndPlay("stopped_up");
+				break;
+			case "down":
+				this.gotoAndPlay("stopped_down");
+				break;
 			}
-    	}
+			
+			this.lastDirection = "stopped";
+		}
     	
     	this.countTimeForWalk++;
     	
